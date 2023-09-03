6770703a389d4d94afd61f23418366ea22bc7b98
Evelyn Pérez <106790102+evperezf@users.noreply.github.com>
2023-09-03 20:00:31 -0300

    autentifica

--- myApp/src/app/usuario/usuario.page.ts
+++ myApp/src/app/usuario/usuario.page.ts
@@ -18,8 +18,8 @@ export class UsuarioPage implements OnInit {
   userInfoReceived: UserModel | undefined;
   idUserHtmlRouterLink: any;
 
-  constructor(private route: Router, private activatedRoute: ActivatedRoute) {
-    this.userInfoReceived = this.route.getCurrentNavigation()?.extras.state?.['user'];
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.userInfoReceived = this.router.getCurrentNavigation()?.extras.state?.['user'];
     // Si quiero obtener un valor por URL usando routerLink
     this.idUserHtmlRouterLink = this.activatedRoute.snapshot.params['id'];
     // Obteniendo el ID podria buscar en algún arreglo o BD el usuario con el id
