a3ab2dd74f57cfa7a7bf3c2b40f51b1c1cd3ed1d
Evelyn Pérez <106790102+evperezf@users.noreply.github.com>
2023-10-15 19:11:38 -0300

    connectionBBDD

---
+++ myApp/src/app/service/registrapp.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RegistrappService } from './registrapp.service';
+
+describe('RegistrappService', () => {
+  let service: RegistrappService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RegistrappService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- myApp/src/environments/environment.ts
+++ myApp/src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeXZ5dmZsZWZiZmZpZHBleHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0NzExMzIsImV4cCI6MjAxMTA0NzEzMn0.i1LUnYRAvNM5Hi5brRbJmuRxmxfHYOlYZwnGekrgfVU',
+   supabaseUrl:'https://bfyvyvflefbffidpexsg.supabase.co'
+  }
 };
-
+/*supabase -> corresponde a la declaracion de la var de la base de datos
+    publicKey-> corresponde an la clave existente en supabase, referenta a la 
+    bbdd que se creo previamente
+    url-> correponde a la url donde esta nuestra bbdd*/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
