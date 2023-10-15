687abfe85826856413202abaee281be974d94686
Evelyn Pérez <106790102+evperezf@users.noreply.github.com>
2023-10-15 20:15:28 -0300

    service

--- myApp/src/app/service/registrapp.service.ts
+++ myApp/src/app/service/registrapp.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrappService {
+  
+  supabase: SupabaseClient;
+  private_currentUser: BehaviorSubject<any> = new BehaviorSubject(null);
 
-  constructor() { }
+  constructor() { 
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey, {
+      auth:{
+      autoRefreshToken: true,
+      persistSession: true
+    }
+});
+
+  this.supabase.auth.onAuthStateChange((event, session)=> { 
+    console.log('event', event);
+    if(event =='SIGNED_IN'){
+      this.private_currentUser.next(session?.user)
+    }else{
+      this.private_currentUser.next(false)
+    }
+  })
+  }
+  
+  loadUser(){
+    
+  }
+  singUp(){
+
+  }
+  singIn(){
+    
+  }
 }
--- myApp/src/environments/environment.ts
+++ myApp/src/environments/environment.ts
@@ -4,10 +4,9 @@
 
 export const environment = {
   production: false,
-  supabase: {
-   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeXZ5dmZsZWZiZmZpZHBleHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0NzExMzIsImV4cCI6MjAxMTA0NzEzMn0.i1LUnYRAvNM5Hi5brRbJmuRxmxfHYOlYZwnGekrgfVU',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeXZ5dmZsZWZiZmZpZHBleHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0NzExMzIsImV4cCI6MjAxMTA0NzEzMn0.i1LUnYRAvNM5Hi5brRbJmuRxmxfHYOlYZwnGekrgfVU',
    supabaseUrl:'https://bfyvyvflefbffidpexsg.supabase.co'
-  }
+  
 };
 /*supabase -> corresponde a la declaracion de la var de la base de datos
     publicKey-> corresponde an la clave existente en supabase, referenta a la 
